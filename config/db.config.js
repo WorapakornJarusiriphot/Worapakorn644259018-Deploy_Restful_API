e2cba9f83499c17e8985027ee15b955c4d7db716
worapakorn <pupupuwora@gmail.com>
2023-09-11 00:48:44 +0700

    Update db.config and package.json
    Update db.config have HOST USER PASSWORD DB and package.json

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "aws.connect.psdb.cloud",
-  USER: "lxg3tpyby09d5t3zmt71", //root เป็นusername ในการต่อ database
-  PASSWORD: "pscale_pw_Q4TGbp8bM88V72U3SUUDCzI9LGTTOejcsdBnA6F6nbK", //ถ้าใช้ XAMPP ก็ไม่ต้องมี PASSWORD เช่น PASSWORD: "" //ถ้าใช้ แอปเซิร์ฟ(AppServ) ก็จะมี PASSWORD เช่น PASSWORD: "12345678"
+  USER: "8abznnxy11gttt105df5", //root เป็นusername ในการต่อ database
+  PASSWORD: "pscale_pw_zrnflN3C76JVzTjYVrIcjXfQeYoayvONUk2md4mFc1U", //ถ้าใช้ XAMPP ก็ไม่ต้องมี PASSWORD เช่น PASSWORD: "" //ถ้าใช้ แอปเซิร์ฟ(AppServ) ก็จะมี PASSWORD เช่น PASSWORD: "12345678"
   DB: "se_database", //เป็นการตั้ง DATABASE ให้มีชื่อว่า restaurants
 };
 
