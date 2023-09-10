42b1ff056b795596141ffb068628a63ad07ed5ca
WorapakornJarusiriphot <120018217+WorapakornJarusiriphot@users.noreply.github.com>
2023-09-11 01:04:18 +0700

    Update db.config
    Update db.config have HOST USER PASSWORD DB

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "aws.connect.psdb.cloud",
-  USER: "qf46768gkzhwigfddgoa", //root เป็นusername ในการต่อ database
-  PASSWORD: "pscale_pw_k5O2irqxem5N0fkYIPLc3eiR6d7j2o7sOxj7wybwlS6", //ถ้าใช้ XAMPP ก็ไม่ต้องมี PASSWORD เช่น PASSWORD: "" //ถ้าใช้ แอปเซิร์ฟ(AppServ) ก็จะมี PASSWORD เช่น PASSWORD: "12345678"
+  USER: "0xt32pkm5it9u9pudhj0", //root เป็นusername ในการต่อ database
+  PASSWORD: "pscale_pw_pw75PYUIYkHA0JOL2WO9EkRBCTktSQgBIXMdD6twnDE", //ถ้าใช้ XAMPP ก็ไม่ต้องมี PASSWORD เช่น PASSWORD: "" //ถ้าใช้ แอปเซิร์ฟ(AppServ) ก็จะมี PASSWORD เช่น PASSWORD: "12345678"
   DB: "se_database", //เป็นการตั้ง DATABASE ให้มีชื่อว่า restaurants
 };
 
