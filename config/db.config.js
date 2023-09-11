fc29346513dec46e1e6d58fd5946a8ea30f938d5
worapakorn <pupupuwora@gmail.com>
2023-09-11 09:33:55 +0700

    Update db.config

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
-  HOST: "aws.connect.psdb.cloud",
-  USER: "qf46768gkzhwigfddgoa", //root เป็นusername ในการต่อ database
-  PASSWORD: "pscale_pw_k5O2irqxem5N0fkYIPLc3eiR6d7j2o7sOxj7wybwlS6", //ถ้าใช้ XAMPP ก็ไม่ต้องมี PASSWORD เช่น PASSWORD: "" //ถ้าใช้ แอปเซิร์ฟ(AppServ) ก็จะมี PASSWORD เช่น PASSWORD: "12345678"
-  DB: "se_database", //เป็นการตั้ง DATABASE ให้มีชื่อว่า restaurants
+  HOST: process.env.HOST,
+  USER: process.env.USER, //root เป็นusername ในการต่อ database
+  PASSWORD: process.env.PASSWORD, //ถ้าใช้ XAMPP ก็ไม่ต้องมี PASSWORD เช่น PASSWORD: "" //ถ้าใช้ แอปเซิร์ฟ(AppServ) ก็จะมี PASSWORD เช่น PASSWORD: "12345678"
+  DB: process.env.DB, //เป็นการตั้ง DATABASE ให้มีชื่อว่า restaurants
 };
 
 //config คือเป็นการตั้งค่าต่างๆ
